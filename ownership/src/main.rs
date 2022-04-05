726b530163e4624d9b07f4644994109c090f51c1
Hawzen <hawzen@users.noreply.github.com>
2022-04-06 02:53:08 +0300

    Update ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,5 +8,13 @@ fn main() {
     println!("{}", **(&&s));
 
     let str_slice: &str = &s[1..];
-    println!("{}", str_slice)
+    println!("{}", str_slice);
+
+    
+    let mut s1 = String::from("Test ");
+    {
+        let s2 = "me?";
+        s1.push_str(s2);
+    }
+    println!("{}", s1);
 }
