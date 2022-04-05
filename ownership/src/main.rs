b91f06293f16aeeb86a7f6f0ba91ca210751041e
Hawzen <hawzen@users.noreply.github.com>
2022-04-05 22:37:58 +0300

    ownership section

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,12 @@
+
 fn main() {
     let s = String::from("str me plz");
-    println!("{}", s.to_string());
+    println!("{}", &&s);
+    println!("{}", &s);
+    println!("{}", s);
 
-    
+    println!("{}", **(&&s));
+
+    let str_slice: &str = &s[1..];
+    println!("{}", str_slice)
 }
